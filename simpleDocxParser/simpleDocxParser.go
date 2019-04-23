d1161bba18704cd6d348a2bb51cde91f11bf91c0
Leon Grünewald <l_gruenewald@web.de>
2019-04-23 12:55:33 +0200

    Reformat code

--- simpleDocxParser/simpleDocxParser.go
+++ simpleDocxParser/simpleDocxParser.go
@@ -35,9 +35,10 @@ var htmlElementAliases = map[string]string{
 	"Heading 4": "<h5>%s</h5>",
 }
 
-func New(file string, pgs *programSettings.ProgramSettings) (doc Document, err error) {
-	doc.styles = make(map[string]string)
-	doc.linkRelations = make(map[string]string)
+func New(file string, pgs *programSettings.ProgramSettings) (doc *Document, err error) {
+	newDoc := Document{}
+	newDoc.styles = make(map[string]string)
+	newDoc.linkRelations = make(map[string]string)
 
 	tempcounter := pgs.Get("tempcounter").(int)
 
@@ -48,51 +49,57 @@ func New(file string, pgs *programSettings.ProgramSettings) (doc Document, err e
 	// To cast interfaces use interface{}.(type)
 	// If you have a basic type like int you can use type(int) to cast it
 
-	doc.originalPath = file
-	doc.tempPath = path.Join("/tmp/docx2cleanhtml/", hex.EncodeToString(md5hasher.Sum(nil)) + string(tempcounter))
-	doc.pgs = pgs
+	newDoc.originalPath = file
+	newDoc.tempPath = path.Join("/tmp/docx2cleanhtml/", hex.EncodeToString(md5hasher.Sum(nil))+string(tempcounter))
+	newDoc.pgs = pgs
 
-	pgs.Set("tempcounter",  + 1)
+	pgs.Set("tempcounter", tempcounter+1)
 
-	folderErr := os.MkdirAll(doc.tempPath, 0750)
+	folderErr := os.MkdirAll(newDoc.tempPath, 0750)
 	zipReader, zipErr := zip.OpenReader(file)
 
-	if zipErr == nil {
-		if folderErr == nil {
-			for _, file := range zipReader.File {
-				if isAcceptedFile(file.Name) {
-					ofHandle, ofErr := os.OpenFile(path.Join(doc.tempPath, path.Base(file.Name)), os.O_WRONLY|os.O_CREATE, 0750)
-					fdHandle, fdErr := file.Open()
-					if fdErr == nil {
-						if ofErr == nil {
-							_, copyErr := io.Copy(ofHandle, fdHandle)
-							if copyErr != nil {
-								log.Fatal(copyErr.Error())
-							}
-							ofcErr := ofHandle.Close()
-							if ofcErr != nil {
-								log.Fatal(ofcErr.Error())
-							}
-						} else {
-							err = ofErr
-						}
-						fccErr := fdHandle.Close()
-						if fccErr != nil {
-							log.Fatal(fccErr.Error())
-						}
-					} else {
-						log.Fatal(fdErr.Error())
-					}
-				}
-			}
-		} else {
-			log.Fatal(folderErr.Error())
-		}
-	} else {
+	if zipErr != nil {
 		log.Fatal(zipErr.Error())
 	}
 
-	return doc, err
+	if folderErr != nil {
+		log.Fatal(folderErr.Error())
+	}
+
+	for _, file := range zipReader.File {
+
+		if !isAcceptedFile(file.Name) {
+			continue
+		}
+
+		ofHandle, err := os.OpenFile(path.Join(newDoc.tempPath, path.Base(file.Name)), os.O_WRONLY|os.O_CREATE, 0750)
+		if err != nil {
+			return nil, err
+		}
+
+		fdHandle, err := file.Open()
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = io.Copy(ofHandle, fdHandle)
+		if err != nil {
+			return nil, err
+		}
+
+		err = ofHandle.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		err = fdHandle.Close()
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return &newDoc, err
 }
 
 func isAcceptedFile(filename string) bool {
@@ -110,73 +117,102 @@ func isAcceptedFile(filename string) bool {
 	return false
 }
 
-func (doc *Document) ReadRelations() {
-	doc.readDocuments()
-	doc.readStyles()
-	doc.getLinkRelations()
-	doc.close()
+func (doc *Document) ReadRelations() error {
+	err := doc.readDocuments()
+	if err != nil {
+		return err
+	}
+
+	err = doc.readStyles()
+	if err != nil {
+		return err
+	}
+
+	err = doc.getLinkRelations()
+	if err != nil {
+		return err
+	}
+
+	err = doc.close()
+	if err != nil {
+		return err
+	}
 }
 
-func (doc *Document) readStyles() {
-	file, fileErr := os.Open(path.Join(doc.tempPath, "styles.xml"))
+func (doc *Document) readStyles() error {
 	var parsedStyles xmlStyles
-	if fileErr == nil {
-		readAllContent, readAllErr := ioutil.ReadAll(file)
-		if readAllErr == nil {
-			parseErr := xml.Unmarshal(readAllContent, &parsedStyles)
-			if parseErr != nil {
-				log.Fatal(parseErr)
-			}
-
-			for _, style := range parsedStyles.Xstyles {
-				doc.styles[style.XstyleId] = style.Xname.Xval
-			}
-		}
+	file, err := os.Open(path.Join(doc.tempPath, "styles.xml"))
+	if err != nil {
+		return err
 	}
+
+	readAllContent, err := ioutil.ReadAll(file)
+	if err == nil {
+		return err
+	}
+
+	err = xml.Unmarshal(readAllContent, &parsedStyles)
+	if err != nil {
+		return err
+	}
+
+	for _, style := range parsedStyles.Xstyles {
+		doc.styles[style.XstyleId] = style.Xname.Xval
+	}
+
+	return nil
 }
 
-func (doc *Document) getLinkRelations() {
-	file, fileErr := os.Open(path.Join(doc.tempPath, "document.xml.rels"))
+func (doc *Document) getLinkRelations() error {
 	var relationships xmlRelationships
-	if fileErr == nil {
-		byteContent, readAllErr := ioutil.ReadAll(file)
-		if readAllErr == nil {
-			parseErr := xml.Unmarshal(byteContent, &relationships)
-			if parseErr != nil {
-				log.Fatal(parseErr)
-			}
+	file, err := os.Open(path.Join(doc.tempPath, "document.xml.rels"))
 
-			for _, rl := range relationships.Xrelationships {
-				doc.pgs.VerbosePrintf("I've found relationship %s (%s)\n", rl.Xtarget, rl.Xid)
-				doc.linkRelations[rl.Xid] = rl.Xtarget
-			}
-		} else {
-			log.Fatal(readAllErr)
-		}
-	} else {
-		log.Fatal(fileErr.Error())
+	if err != nil {
+		return err
 	}
+
+	byteContent, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return err
+	}
+
+	err = xml.Unmarshal(byteContent, &relationships)
+	if err != nil {
+		return err
+	}
+
+	for _, rl := range relationships.Xrelationships {
+		doc.pgs.VerbosePrintf("I've found relationship %s (%s)\n", rl.Xtarget, rl.Xid)
+		doc.linkRelations[rl.Xid] = rl.Xtarget
+	}
+
+	return nil
 }
 
-func (doc *Document) readDocuments() {
-	file, fileErr := os.Open(path.Join(doc.tempPath, "document.xml"))
-	if fileErr == nil {
-		byteContent, readAllErr := ioutil.ReadAll(file)
-		if readAllErr == nil {
-			parseErr := xml.Unmarshal(byteContent, &doc.parsedDocument)
-			if parseErr != nil {
-				log.Fatal(parseErr)
-			}
-		} else {
-			log.Fatal(readAllErr)
-		}
-	} else {
-		log.Fatal(fileErr.Error())
+func (doc *Document) readDocuments() error {
+	file, err := os.Open(path.Join(doc.tempPath, "document.xml"))
+
+	if err != nil {
+		return err
 	}
+
+	byteContent, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return err
+	}
+
+	err = xml.Unmarshal(byteContent, &doc.parsedDocument)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (doc *Document) close() {
-	os.RemoveAll(doc.tempPath)
+func (doc *Document) close() error {
+	return os.RemoveAll(doc.tempPath)
 }
 
 func (doc *Document) HTML() string {
--- simpleDocxParser/xmlLinks.go
+++ simpleDocxParser/xmlLinks.go
@@ -3,13 +3,13 @@ package simpleDocxParser
 import "encoding/xml"
 
 type xmlRelationships struct {
-	XMLName xml.Name `xml:"Relationships"`
+	XMLName        xml.Name          `xml:"Relationships"`
 	Xrelationships []xmlRelationship `xml:"Relationship"`
 }
 
 type xmlRelationship struct {
 	XMLName xml.Name `xml:"Relationship"`
-	Xid string `xml:"Id,attr"`
-	Xtype string `xml:"Type,attr"`
-	Xtarget string `xml:"Target,attr"`
+	Xid     string   `xml:"Id,attr"`
+	Xtype   string   `xml:"Type,attr"`
+	Xtarget string   `xml:"Target,attr"`
 }
